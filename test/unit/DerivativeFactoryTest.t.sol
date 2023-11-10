7a0a55f16334ed8dca857b023f24f09f45575ef2
palmcivet7 <palmcivet@proton.me>
2023-11-10 00:47:14 +0000

    test constructor

--- test/unit/DerivativeFactoryTest.t.sol
+++ test/unit/DerivativeFactoryTest.t.sol
@@ -5,12 +5,14 @@ pragma solidity ^0.8.19;
 import {Test, console} from "forge-std/Test.sol";
 import {DeployDerivativeFactory} from "../../script/DeployDerivativeFactory.s.sol";
 import {DerivativeFactory} from "../../src/DerivativeFactory.sol";
+import {CustomDerivative} from "../../src/CustomDerivative.sol";
 import {Vm} from "forge-std/Vm.sol";
 import {MockV3Aggregator} from "../mocks/MockV3Aggregator.sol";
 import {MockUSDC} from "../mocks/MockUSDC.sol";
 
 contract DerivativeFactoryTest is Test {
     DerivativeFactory derivativeFactory;
+    CustomDerivative customDerivative;
 
     uint8 public constant DECIMALS = 8;
     int256 public constant ASSET_USD_PRICE = 2000e8;
@@ -23,6 +25,13 @@ contract DerivativeFactoryTest is Test {
     bool long = true;
     bool notLong;
 
+    address customLong;
+    address customShort;
+
+    address public PARTY_A = makeAddr("PARTY_A");
+    address public PARTY_B = makeAddr("PARTY_B");
+    address public RANDOM_USER = makeAddr("RANDOM_USER");
+
     function setUp() external {
         DeployDerivativeFactory deployer = new DeployDerivativeFactory();
         (derivativeFactory) = deployer.run();
@@ -33,18 +42,30 @@ contract DerivativeFactoryTest is Test {
     }
 
     modifier longContract() {
-        derivativeFactory.createCustomDerivative(
+        vm.prank(PARTY_A);
+        customLong = derivativeFactory.createCustomDerivative(
             priceFeed, STRIKE_PRICE, settlementTime, collateralToken, COLLATERAL_AMOUNT, long
         );
+        customDerivative = CustomDerivative(customLong);
         _;
     }
 
     modifier shortContract() {
-        derivativeFactory.createCustomDerivative(
+        vm.prank(PARTY_A);
+        customShort = derivativeFactory.createCustomDerivative(
             priceFeed, STRIKE_PRICE, settlementTime, collateralToken, COLLATERAL_AMOUNT, notLong
         );
+        customDerivative = CustomDerivative(customShort);
         _;
     }
 
-    function testConstructorPropertiesSetCorrectly() public longContract {}
+    function testConstructorPropertiesSetCorrectly() public longContract {
+        assertEq(customDerivative.partyA(), address(PARTY_A));
+        assertEq(address(customDerivative.priceFeed()), priceFeed);
+        assertEq(customDerivative.strikePrice(), STRIKE_PRICE);
+        assertEq(customDerivative.settlementTime(), settlementTime);
+        assertEq(address(customDerivative.collateralToken()), collateralToken);
+        assertEq(customDerivative.collateralAmount(), COLLATERAL_AMOUNT);
+        assertEq(customDerivative.isPartyALong(), long);
+    }
 }
