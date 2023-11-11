3a995a5664d28ab734806fc78ab07ab658fa49c1
palmcivet7 <palmcivet@proton.me>
2023-11-11 15:12:10 +0000

    add natspec

--- src/DerivativeFactory.sol
+++ src/DerivativeFactory.sol
@@ -4,6 +4,20 @@ pragma solidity ^0.8.19;
 
 import {CustomDerivative} from "./CustomDerivative.sol";
 
+/**
+ * @title DerivativeFactory
+ * @author palmcivet.eth
+ *
+ * This is the contract that allows users to deploy their own versions of our CustomDerivative contract.
+ * When a user deploys their CustomDerivative contract they will specify the following parameters:
+ *  - underlying asset
+ *  - strike price (the final price being above or below this will determine the party receiving the payout)
+ *  - settlement time (the time the underlying asset's price will be compared to the strike price)
+ *  - collateral asset (ERC20 token, USDC is recommended for demonstration purposes)
+ *  - collateral amount (amount of collateral to be deposited by both parties)
+ *  - long or short position (the deploying user will choose their position and the counterparty will take the opposite)
+ */
+
 contract DerivativeFactory {
     event DerivativeCreated(address derivativeContract, address partyA);
 
