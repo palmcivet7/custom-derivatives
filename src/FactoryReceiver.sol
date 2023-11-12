10065fad9fc3d007ef618934b336d9f80c76cbb9
palmcivet7 <palmcivet@proton.me>
2023-11-12 14:09:25 +0000

    declare decoded variables inside function before decoding

--- src/FactoryReceiver.sol
+++ src/FactoryReceiver.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.19;
 
 import "./DerivativeFactory.sol";
@@ -10,14 +9,16 @@ contract FactoryReceiver is DerivativeFactory, CCIPReceiver {
     constructor(address _router) CCIPReceiver(_router) {}
 
     function _ccipReceive(Client.Any2EVMMessage memory message) internal override {
-        (
-            address _priceFeed,
-            uint256 _strikePrice,
-            uint256 _settlementTime,
-            address _collateralToken,
-            uint256 _collateralAmount,
-            bool _isPartyALong
-        ) = abi.decode(message.data, (address, uint256, uint256, address, uint256, bool));
+        address _priceFeed;
+        uint256 _strikePrice;
+        uint256 _settlementTime;
+        address _collateralToken;
+        uint256 _collateralAmount;
+        bool _isPartyALong;
+
+        (_priceFeed, _strikePrice, _settlementTime, _collateralToken, _collateralAmount, _isPartyALong) =
+            abi.decode(message.data, (address, uint256, uint256, address, uint256, bool));
+
         createCustomDerivative(
             _priceFeed, _strikePrice, _settlementTime, _collateralToken, _collateralAmount, _isPartyALong
         );
