66c4fa6925f3018b55198d918a479356607ce1cd
palmcivet7 <palmcivet@proton.me>
2023-12-06 10:48:26 +0000

    remove pricefeed address

--- script/v1-data-feeds/HelperSenderConfig.s.sol
+++ script/v1-data-feeds/HelperSenderConfig.s.sol
@@ -12,7 +12,6 @@ contract HelperSenderConfig is Script {
     struct NetworkConfig {
         address link;
         address router;
-        address priceFeed;
     }
 
     NetworkConfig public activeNetworkConfig;
@@ -30,16 +29,14 @@ contract HelperSenderConfig is Script {
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         return NetworkConfig({
             link: 0x779877A7B0D9E8603169DdbD7836e478b4624789, // https://sepolia.etherscan.io/token/0x779877a7b0d9e8603169ddbd7836e478b4624789
-            router: 0xD0daae2231E9CB96b94C8512223533293C3693Bf, // https://docs.chain.link/ccip/supported-networks#ethereum-sepolia
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 // https://docs.chain.link/data-feeds/price-feeds/addresses?network=ethereum&page=1#sepolia-testnet
+            router: 0xD0daae2231E9CB96b94C8512223533293C3693Bf // https://docs.chain.link/ccip/supported-networks#ethereum-sepolia
         });
     }
 
     function getFujiConfig() public pure returns (NetworkConfig memory) {
         return NetworkConfig({
             link: 0x0b9d5D9136855f6FEc3c0993feE6E9CE8a297846, // https://testnet.snowtrace.io/token/0x0b9d5d9136855f6fec3c0993fee6e9ce8a297846
-            router: 0x554472a2720E5E7D5D3C817529aBA05EEd5F82D8, // https://docs.chain.link/ccip/supported-networks#avalanche-fuji
-            priceFeed: 0x5498BB86BC934c8D34FDA08E81D444153d0D06aD // https://docs.chain.link/data-feeds/price-feeds/addresses?network=avalanche&page=1#avalanche-testnet
+            router: 0x554472a2720E5E7D5D3C817529aBA05EEd5F82D8 // https://docs.chain.link/ccip/supported-networks#avalanche-fuji
         });
     }
 
@@ -48,7 +45,6 @@ contract HelperSenderConfig is Script {
         MockARM mockArm = new MockARM();
         Router router = new Router(address(weth9), address(mockArm));
         MockLinkToken mockLink = new MockLinkToken();
-        address priceFeed;
-        return NetworkConfig({link: address(mockLink), router: address(router), priceFeed: address(priceFeed)});
+        return NetworkConfig({link: address(mockLink), router: address(router)});
     }
 }
